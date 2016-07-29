ddfbe3d16e427b07cb4317b88dd89824556fa1e6
andressamarques <andressa.alves.aa.457@gmail.com>
2016-07-29 09:53:06 -0300

    Quadrado Perfeito
    Trabalho Antoanne (é o brilho de teu olhar)
    Descobrir se um num. é quadrado perfeito.

---
+++ Número Perfeito/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado_Perfeito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Digite um número:");
+            num = int.Parse(Console.ReadLine()); 
+            
+            int soma = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    soma += i;
+                }
+                
+            }
+
+            if (soma == num)
+            {
+
+                Console.WriteLine("Numero Perfeito");
+            }
+            else
+            {
+                Console.WriteLine("Numero n perfeito");
+
+            }
+                Console.ReadKey();
+            }
+
+        }
+    }
+
